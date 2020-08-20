cb7dd57ba21a39a082aa2ad40643809c7d708c73
Steven Kim <97stevenkim@gmail.com>
2020-08-20 17:57:23 -0400

    merged

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React from 'react';
->>>>>>> bd888c44e20cd1a51d3e1730e876cfe9dc4fc90c
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="main">
-      <div className="title">WeatherApp</div>
-      <input type="text"></input>
-      <button>Search</button>
-=======
     <div>
       <div className="title">
         WeatherApp
@@ -20,7 +10,6 @@ function App() {
         <input className="searchfield" type="text" />
         <button className="searchbarbutton" text="Find Forecast" />
       </div>
->>>>>>> c01e20831d6884884d3858986bd148efcb10bf15
     </div>
   );
 }
