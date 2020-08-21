d68df141c527ffde7520b8d063c7b0aee971e7de
Steven Kim <97stevenkim@gmail.com>
2020-08-21 17:47:41 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+var api_key = '8e7b5a4d1f58b0476c0658c7c172e7af'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <input className="searchfield" type="text" value="Location" />
         <button className="searchbarbutton">Find Forecast</button>
       </div>
-
+      http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={api_key}
       <MainForecast />
       <LongForecast />
     </div>
