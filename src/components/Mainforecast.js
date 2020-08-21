9f1656909ce8034d5832db3d9b589df645147d69
Adderall-dev <urbaniak_edwin@wp.pl>
2020-08-22 00:05:27 +0200

    e casdasd

--- src/components/Mainforecast.js
+++ src/components/Mainforecast.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../Styles/Mainforecast.css";
 
 function Mainforecast() {
   return (
-    <>
+    <div className="main">
       <h1>Temperature: 21 celcius</h1>
       <h3>city: New york</h3>
-    </>
+    </div>
   );
 }
 
