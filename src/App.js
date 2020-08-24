dcf8e9584d1c51233e836b820d8ccd41a1ec91d1
Adderall-dev <urbaniak_edwin@wp.pl>
2020-08-24 18:52:13 +0200

    e fix for render

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import React, { useState } from "react";
 import api_key from "./api/openweather";
 import Longforecast from "./Components/Longforecast";
 import Mainforecast from "./Components/Mainforecast";
-import openweather from './api/openweather';
-
-
-
-
+import openweather from "./api/openweather";
 
 function App() {
-
   const getWeather = () => {
-    console.log('hey whats up')
-  }
+    console.log("hey whats up");
+  };
   const searchbarChanged = (e) => {
     // look through cities.txt and show first 3 results that start with e.target.value
-  })
+  };
 
-}
-const [weatherData, setWeatherData] = useState(() => "test")
-return (
-  <div>
-    <div className="title">WeatherApp</div>
-    <div className="searchbardiv">
-      <input onChange={searchbarChanged} className="searchfield" type="text" placeholder="Location" />
-      <button onClick={getWeather} className="searchbarbutton">Find Forecast</button>
+  const [weatherData, setWeatherData] = useState(() => "test");
+  return (
+    <div>
+      <div className="title">WeatherApp</div>
+      <div className="searchbardiv">
+        <input
+          onChange={searchbarChanged}
+          className="searchfield"
+          type="text"
+          placeholder="Location"
+        />
+        <button onClick={getWeather} className="searchbarbutton">
+          Find Forecast
+        </button>
+      </div>
+      <Mainforecast />
+      <Longforecast />
     </div>
-    <Mainforecast />
-    <Longforecast />
-  </div>
-);
+  );
 }
-
 export default App;
